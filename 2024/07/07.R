6c7f814608e9d84f321a60bd2b9e8bf29554724e
andrés <acastroaraujo@gmail.com>
2024-12-08 21:50:39 -0500

    a

--- 2024/07/07.R
+++ 2024/07/07.R
@@ -12,25 +12,25 @@ input <- map(lines, \(x) x[-1])
 
 # Part 1 ------------------------------------------------------------------
 
-run <- function(x, target) {
+process <- function(x, target) {
   
   if (x[[1]] > target | !is.finite(x[[1]])) return(FALSE)
   if (length(x) == 1) return(ifelse(x == target, TRUE, FALSE))
   
   out <- detect_index(c("+", "*"), function(f) {
     r <- do.call(f, as.list(x[1:2]))
-    run(c(r, x[-(1:2)]), target)
+    process(c(r, x[-(1:2)]), target)
   }) 
   
   out > 0
 }
 
-i <- furrr::future_map2_lgl(input, output, run, .progress = TRUE) 
+i <- furrr::future_map2_lgl(input, output, process, .progress = TRUE) 
 sum(output[i])
 
 # Part 2 ------------------------------------------------------------------
 
-run <- function(x, target) {
+process <- function(x, target) {
   
   if (x[[1]] > target | !is.finite(x[[1]])) return(FALSE)
   if (length(x) == 1) return(ifelse(x == target, TRUE, FALSE))
@@ -39,12 +39,12 @@ run <- function(x, target) {
   
   out <- detect_index(c("+", "*", "%c%"), function(f) {
     r <- do.call(f, as.list(x[1:2]))
-    run(c(r, x[-(1:2)]), target)
+    process(c(r, x[-(1:2)]), target)
   }) 
   
   out > 0
 }
 
-i <- furrr::future_map2_lgl(input, output, run, .progress = TRUE) 
-format(sum(output[i]), scientific = FALSE)
+i <- furrr::future_map2_lgl(input, output, process, .progress = TRUE) 
+as.character(sum(output[i]))
 
